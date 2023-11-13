53c8f2498062cf332dad8606440a11a02f544d41
Uri Faynberg <uri.faynberg@c2a-sec.com>
2023-11-13 12:23:51 +0200

    elk crashing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@
 import CytoscapeComponent from 'react-cytoscapejs'
 import { nodeElements } from './nodeElements'
 import { stylesheet } from './stylesheet'
+import { optimizeGraph } from './optimizer'
+import { useEffect, useState } from 'react';
 
 const App = () => {
+  // let changedNodeElements = optimizeGraph(nodeElements);
+  
+  const [cy, setCy] = useState(null);
+  useEffect(() => {
+    if (cy) {
+      // let bc = cy.elements().betweennessCentrality();
+      optimizeGraph(cy, nodeElements);
+    }
+  }, [cy]);
   return (
     <CytoscapeComponent
       stylesheet={stylesheet.elements}
       style={stylesheet.base}
+      cy={cyRef => setCy(cyRef)}
       elements={nodeElements}
+      layout={stylesheet.layout}
     />
   )
 }
---
+++ src/optimizer.js
@@ -0,0 +1,81 @@
+import cytoscape from 'cytoscape'
+import { stylesheet } from './stylesheet';
+
+const isEdge = (element) => {
+    return ('source' in element.data) && ('target' in element.data);
+}
+
+const getEdges = (nodeElements) => {
+    return nodeElements.filter(isEdge);
+}
+
+
+
+const getMatrixDimensions = () => {
+    let widthMax = 0;
+    let heightMax = 0;
+    stylesheet.elements.forEach((val) => {
+        widthMax = Math.max(widthMax, parseInt(val.style.width ?? 0));
+        widthMax = Math.max(heightMax, parseInt(val.style.height ?? 0));
+    })
+    let baseWidth = parseInt(stylesheet.base.width);
+    let baseHeight = parseInt(stylesheet.base.height);
+    const padFactor = 2;
+    let cell = {
+        width: widthMax * padFactor,
+        height: heightMax * padFactor    
+    };
+    let matrix = {
+        width: Math.floor(baseWidth / cell.width),
+        height: Math.floor(baseHeight / cell.height)
+    };
+    let center = {
+        width: Math.floor(matrix.width / 2),
+        height: Math.floor(matrix.height / 2)
+    };
+
+    return { matrix: matrix, cell: cell, center: center };
+}
+
+var cnt = 0;
+export const optimizeGraph = (cy, nodeElements) => {
+    if (cnt++) {
+        return;
+    }
+    // let edges = getEdges(nodeElements);
+
+    // nodeElements[0].position.x = 500;
+
+    let bc = cy.$().bc();
+    let nodes = cy.nodes();
+    // nodes.forEach((ele, i, eles) => {
+    //     console.log('bc for ', ele.id(), ' is ', bc.betweenness(ele));
+    // });
+    let sortedNodes = nodes.sort((a,b) => { return bc.betweenness(b)-bc.betweenness(a); });
+    sortedNodes.forEach((ele, i, eles) => {
+        console.log('bc for ', ele.id(), ' is ', bc.betweenness(ele));
+    });
+
+    // let dimensions = getMatrixDimensions();
+    // matrix = new Map([[dimensions.center, ]]);
+
+    // let layoutParams = {
+    //     fit: false,
+    //     padding: 100,
+    //     animate: false,
+    //     animationDuration: 500,
+    //     name: 'elk',
+    //     // quality: 'proof', // : 'default',
+    //     quality: 'default',
+    //     randomize: true,
+    //     nodeDimensionsIncludeLabels: true,
+    //     elk: {
+    //       algorithm: 'stress',
+    //       'stress.epsilon': 0.1, // Adjust the minimum allowed distance between nodes
+    //       'stress.desiredEdgeLength': 100.0,
+    //     }
+    // };
+    // let layout = cy.layout(layoutParams);
+    // layout.run();
+    
+}
