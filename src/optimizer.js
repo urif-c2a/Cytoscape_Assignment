b7196b6c9b59e77c9ae9c3c82d8d51b162085472
Uri Faynberg <uri.faynberg@c2a-sec.com>
2024-01-11 17:45:39 +0200

    optimizer save

--- src/optimizer.js
+++ src/optimizer.js
@@ -62,45 +62,87 @@ export const optimizeGraph = (cy, nodeElements) => {
     // nodes.forEach((ele, i, eles) => {
     //     console.log('bc for ', ele.id(), ' is ', bc.betweenness(ele));
     // });
-    let sortedNodes = nodes.sort((a,b) => { return bc.betweenness(b)-bc.betweenness(a); });
-    sortedNodes.forEach((ele, i, eles) => {
-        console.log('bc for ', ele.id(), ' is ', bc.betweenness(ele));
-    });
 
+
+    // let sortedNodes = nodes.sort((a,b) => { return bc.betweenness(b)-bc.betweenness(a); });
+    // sortedNodes.forEach((ele, i, eles) => {
+    //     console.log('bc for ', ele.id(), ' is ', bc.betweenness(ele));
+    // });
+    // let mostCentralNode = sortedNodes[0];
+
+    let centralMax = nodes.max((ele, i, eles) => { return bc.betweenness(ele); });
+    let mostCentralNode = centralMax.ele;
+    console.log('mostCentralNode ', mostCentralNode.id(), ' bc is ', bc.betweenness(mostCentralNode));
     // let dimensions = getMatrixDimensions();
     // matrix = new Map([[dimensions.center, ]]);
+    let fixMostCentral = [{
+        nodeId: mostCentralNode.id(), position: {
+            x: parseInt(cy.width()/2),
+            y: parseInt(cy.height()/2)}
+            // x: parseInt(parseInt(stylesheet.base.width)/2),
+            // y: parseInt(parseInt(stylesheet.base.height)/2)}
+        }];
+    console.log(' cy dimentions: x: ', cy.width(), ', y: ', cy.height());
 
     let layoutParams = {
         fit: true,
         padding: 100,
-        animate: true,
+        animate: false,
         animationDuration: 500,
-        name: 'elk',
-        quality: 'proof', // : 'default',
-        // quality: 'default',
+        quality: 'proof',
         randomize: true,
         nodeDimensionsIncludeLabels: true,
-        elk: {
+        name: 'fcose',
+        // packComponents: true,
+        // nodeSeparation: 100,
+        // piTol: 0.0000001,
+        nodeRepulsion: node => 8000,
+        // idealEdgeLength: edge => 40,
+        edgeElasticity: edge => 0.5,
+        // nestingFactor: 0.5,
+        numIter: 50000,
+        // tile: true,
+        // tilePaddingVertical: 20,
+        // tilePaddingHorizontal: 20,
+        gravity: 0.4,
+        gravityRange: 0.8,
 
-            // algorithm: 'graphviz.neato',
+        // gravityCompound: 1.0,
+        // gravityRangeCompound: 1.5,
+
+        initialEnergyOnIncremental: 0.6,
+        fixedNodeConstraint: fixMostCentral,
+
+        // fit: true,
+        // padding: 100,
+        // animate: true,
+        // animationDuration: 500,
+        // name: 'elk',
+        // quality: 'proof', // : 'default',
+        // // quality: 'default',
+        // randomize: true,
+        // nodeDimensionsIncludeLabels: true,
+        // elk: {
+
+        //     // algorithm: 'graphviz.neato',
             
 
-            algorithm: 'mrtree',
+        //     algorithm: 'mrtree',
 
 
-            // algorithm: 'sporeOverlap',
-            // 'elk.overlapRemoval.maxIterations': 400, // Adjust the minimum allowed distance between nodes
-            // 'elk.spacing.nodeNode': 20,
+        //     // algorithm: 'sporeOverlap',
+        //     // 'elk.overlapRemoval.maxIterations': 400, // Adjust the minimum allowed distance between nodes
+        //     // 'elk.spacing.nodeNode': 20,
 
 
-            // algorithm: 'sporeCompaction',
-            // 'elk.spacing.nodeNode': 400, // Adjust the minimum allowed distance between nodes
-            // 'sporeCompaction.desiredEdgeLength': 40.0,
+        //     // algorithm: 'sporeCompaction',
+        //     // 'elk.spacing.nodeNode': 400, // Adjust the minimum allowed distance between nodes
+        //     // 'sporeCompaction.desiredEdgeLength': 40.0,
 
-            //   algorithm: 'stress',
-        //   'stress.epsilon': 0.1, // Adjust the minimum allowed distance between nodes
-        //   'stress.desiredEdgeLength': 40.0,
-        }
+        //     //   algorithm: 'stress',
+        // //   'stress.epsilon': 0.1, // Adjust the minimum allowed distance between nodes
+        // //   'stress.desiredEdgeLength': 40.0,
+        // }
     };
     let layout = cy.layout(layoutParams);
     layout.run();
